53e51f3156a2b3743e0a27ac2756ae510fecb122
conflagrate <jm@memorici.de>
2022-03-28 20:41:43 +0100

    Problem: Can't test multiple components
    Solution:
    
     - To test multiple React components without editing docker-compose.yaml, we designate a special env variable "RUNTS_ENDPOINT".
     - This variable should be placed in some env file (see how we use _env in test.sh)
     - To demonstrate that it all works, we patched test.sh to check if the variable is set in the second echo instruction of ui container
     - We tidied up docker-compose.yaml, including a patch from do-auth repository: https://github.com/doma-engineering/do-auth/commit/ecabe5c36df3fe5552577edac6764f92217231f8

--- test.sh
+++ test.sh
@@ -2,8 +2,22 @@ cd template/
 eval "$(direnv export bash)"
 
 cp ../_package.json package.json
+echo 'Installing components'
 npm i -D '@wdio/cli@latest' '@wdio/local-runner@latest' '@wdio/mocha-framework@latest' '@wdio/spec-reporter@latest' 'chromedriver@latest' 'tsm@latest' 'wdio-chromedriver-service@latest'
-./wdio.sh && echo 'TEMPLATE TESTS PASSED' || echo 'TEMPLATE TESTS FAILED'
+echo 'Running App.tsx'
+_wdio_app_tsx="$(docker compose up | grep App.tsx | wc -l)"
+echo 'Running EmptyEntrypoint.tsx'
+_wdio_emptyentrypoint_tsx="$(docker compose --env-file ../_env up | grep EmptyEntry | wc -l)"
+if [[ "$_wdio_app_tsx" == "1" ]]; then
+  if [[ "$_wdio_emptyentrypoint_tsx" == "1" ]]; then
+    echo 'Running functionality tests'
+    ./wdio.sh &&  echo 'TEMPLATE TESTS PASSED' || echo 'TEMPLATE TESTS FAILED: wdio.sh launch fault'
+  else
+    echo 'TEMPLATE TESTS FAILED: EmptyEntrypoint.tsx launch fault'
+  fi
+else
+  echo 'TEMPLATE TESTS FAILED: App.tsx launch fault'
+fi
 rm package.json
 rm package*lock*
 rm -rf node_modules
